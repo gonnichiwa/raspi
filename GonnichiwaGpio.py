3735ea371b6aabf6b121e85fb508ea4e853b68c4
gonnichiwa <keepthefaith00@gmail.com>
2020-07-30 14:00:47 +0900

    debug3

--- GonnichiwaGpio.py
+++ GonnichiwaGpio.py
@@ -9,7 +9,7 @@ class GonnichiwaGpio:
         self.GPIO_INPUT = gpioPin_input # 생성자 파라미터
         self.GPIO_LED   = gpio_led
         # pwm SET
-        self.pwm.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BOARD)
         self.pwm        = GPIO.PWM(self.GPIO_LED, pwmHz)
         self.pwm.start(0)
         # pwm dutyRatio Array
