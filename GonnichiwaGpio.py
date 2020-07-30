f14d1c1e761b3675cad98fa7c252a8f8bd0b6825
gonnichiwa <keepthefaith00@gmail.com>
2020-07-30 12:21:44 +0900

    class 구조화 시험

---
+++ GonnichiwaGpio.py
@@ -0,0 +1,8 @@
+
+class GonnichiwaGpio:
+    def __init__(self):
+        self.num = 0
+
+    def getNum(self):
+        num = self.num
+        return num
---
+++ main.py
@@ -0,0 +1,11 @@
+from GonnichiwaGpio import GonnichiwaGpio
+
+def main():
+    print('hihi')
+    gon = GonnichiwaGpio()
+    num = gon.getNum()
+    print('gon.getNum', num)
+
+
+if __name__ == "__main__":
+    main()
