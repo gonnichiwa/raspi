db1bfa2fdf20827acca9f35af25867dbc6ef49d3
gonnichiwa <keepthefaith00@gmail.com>
2020-07-30 14:24:12 +0900

    debug7

--- GonnichiwaGpio.py
+++ GonnichiwaGpio.py
@@ -22,48 +22,45 @@ class GonnichiwaGpio:
         self.dutyValues = []
 
     def __gpioCleanup(self): # private method (객체 외부에서 사용할 수 없다.)
-        GPIO.cleanup()
+        self.pwm.stop()  # pwm stop
+        GPIO.cleanup()   # gpio stop
 
     def __setDutyRatioArray(self, ratio=0.1): # ratio param의 default가 0.1
-        # 듀티 비 목록 작성 (0.1단위로 ratio까지 배열 삽입)
+        # 듀티 ratio 목록 작성 (0.1단위로 ratio 100.0까지 배열 삽입)
         dutyValues = []
         value = ratio
         while value <= 100.0:
-            dutyValues.append(round(value,1))
+            dutyValues.append(round(value,1)) # 0.1 단위로 소수점 1째자리까지 추가.
             value = value + 0.1
             
         return dutyValues
 
-    def __changeDutyCycle(self, dutyArr):
+    def __changeDutyCycle(self, dutyArr=[]):
         for val in dutyArr:
             self.pwm.ChangeDutyCycle(val)
             time.sleep(0.001)
     
     def __ledOnSmoothly(self):
         dutyValues = self.__setDutyRatioArray(ratio=0.1)
+        print(dutyValues)
         self.__changeDutyCycle(dutyValues)
 
     def __ledOffSmoothly(self):
         dutyValues = self.__setDutyRatioArray(ratio=0.1).reverse()
+        print(dutyValues)
         self.__changeDutyCycle(dutyValues)
 
     def start(self):
-        #GPIO.setmode(GPIO.BOARD)
-    
-        #GPIOPIN_7 = self.GPIO_INPUT
-        #LED = self.GPIO_LED
-        
-        
         try:
             while True:
                 # 스위치 입력 판별.
                 key_in = GPIO.input(self.GPIO_INPUT)
                 
                 if key_in == SwitchInput.SWITCH_OPEN:
-                    # TODO: PWM 으로 천천히 켜짐.
+                    # PWM 으로 천천히 켜짐.
                     self.__ledOnSmoothly()
                 else:
-                    # TODO: PWM 으로 천천히 꺼짐.
+                    # PWM 으로 천천히 꺼짐.
                     self.__ledOffSmoothly()
                 
         except BaseException:
